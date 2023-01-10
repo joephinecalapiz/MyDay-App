0cee41f62eaee5335c1e4924e18da1b18b2bbbca
Anthony <salasanthony515@gmail.com>
2023-01-10 15:17:15 +0800

    Fixing Codes on Landing Page

--- lib/pages/LandingPage.dart
+++ lib/pages/LandingPage.dart
@@ -1,29 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:MyDayApp/utils.dart';
 
-
 class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     double baseWidth = 360;
     double fem = MediaQuery.of(context).size.width / baseWidth;
     double ffem = fem * 0.97;
-    return Container(
+    return SizedBox(
       width: double.infinity,
-      child: Container(
-        // landingpagepNM (1:483)
+      child: SizedBox(
         width: double.infinity,
         height: 800*fem,
         child: Stack(
           children: [
             Positioned(
-              // backgroundhKf (1:485)
               left: 0*fem,
               top: 0*fem,
               child: Container(
                 width: 360*fem,
                 height: 800*fem,
-                decoration: BoxDecoration (
+                decoration: const BoxDecoration (
                   color: Color(0xff19191c),
                   image: DecorationImage (
                     fit: BoxFit.cover,
@@ -33,12 +32,11 @@ class LandingPage extends StatelessWidget {
                   ),
                 ),
                 child: Center(
-                  // rectangle38fs (1:487)
                   child: SizedBox(
                     width: double.infinity,
                     height: 800*fem,
                     child: Container(
-                      decoration: BoxDecoration (
+                      decoration: const BoxDecoration (
                         color: Color(0x66000000),
                       ),
                     ),
@@ -47,7 +45,6 @@ class LandingPage extends StatelessWidget {
               ),
             ),
             Positioned(
-              // buttonrbs (104:166)
               left: 281*fem,
               top: 688*fem,
               child: Align(
@@ -69,7 +66,6 @@ class LandingPage extends StatelessWidget {
               ),
             ),
             Positioned(
-              // introXi1 (1:492)
               left: 27*fem,
               top: 136*fem,
               child: Container(
@@ -79,14 +75,12 @@ class LandingPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      // group6Spy (1:496)
                       margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 19*fem),
                       width: 142*fem,
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Container(
-                            // welcometoZub (1:497)
                             margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 14*fem),
                             width: double.infinity,
                             child: Text(
@@ -98,12 +92,11 @@ class LandingPage extends StatelessWidget {
                                 fontWeight: FontWeight.w600,
                                 height: 1*ffem/fem,
                                 letterSpacing: -0.4079999924*fem,
-                                color: Color(0xff0075ff),
+                                color: const Color(0xff0075ff),
                               ),
                             ),
                           ),
                           Text(
-                            // mydaysJ1 (1:498)
                             'MyDay',
                             style: SafeGoogleFont (
                               'Poppins',
@@ -111,14 +104,13 @@ class LandingPage extends StatelessWidget {
                               fontWeight: FontWeight.w600,
                               height: 0.5365853659*ffem/fem,
                               letterSpacing: -0.4079999924*fem,
-                              color: Color(0xff0075ff),
+                              color: const Color(0xff0075ff),
                             ),
                           ),
                         ],
                       ),
                     ),
                     Container(
-                      // wannarecordyourdayyouvechosent (1:495)
                       constraints: BoxConstraints (
                         maxWidth: 206*fem,
                       ),
@@ -129,7 +121,7 @@ class LandingPage extends StatelessWidget {
                           fontSize: 13*ffem,
                           fontWeight: FontWeight.w400,
                           height: 1.5*ffem/fem,
-                          color: Color(0xffffffff),
+                          color: const Color(0xffffffff),
                         ),
                       ),
                     ),
