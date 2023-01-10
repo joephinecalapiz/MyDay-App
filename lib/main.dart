81053a2508deb5140a6e5ae9f68726d9977f1cbb
Anthony <salasanthony515@gmail.com>
2023-01-10 15:11:23 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:MyDayApp/pages/LandingPage.dart';
+import 'package:flutter/material.dart';
+import 'package:MyDayApp/utils.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+	return MaterialApp(
+		title: 'MyDay APP',
+		debugShowCheckedModeBanner: false,
+		scrollBehavior: MyCustomScrollBehavior(),
+		theme: ThemeData(
+		primarySwatch: Colors.blue,
+		),
+		home: Scaffold(
+		body: SingleChildScrollView(
+			child: LandingPage(),
+		),
+		),
+	);
+	}
+}
